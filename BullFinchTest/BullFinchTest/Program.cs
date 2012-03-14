58d8af7331e4aff4dcf95f90bc7f19e94fa02b6b
Kirill <kirill.j.kolesnikov@gmail.com>
2012-03-14 18:30:29 +0700

    Adding first test code for project

---
+++ BullFinchTest/BullFinchTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BullFinchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
